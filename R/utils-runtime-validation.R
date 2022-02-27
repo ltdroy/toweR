d81c1eae1a3183a9d27f126df03fb957ef795149
ltdroy <ltdroy@users.noreply.github.com>
2022-02-27 17:28:43 +0000

    feat: Added standard check for scaffold_df
    Many functions make assumptions about recieving a scaffold_df as an input, this assertion standardises a core part of runtime checks against this assumption

--- R/utils-runtime-validation.R
+++ R/utils-runtime-validation.R
@@ -1,5 +1,12 @@
 is_file_scaffold <- function(ob){
 
-  return(TRUE)
+  isTRUE(assertthat::assert_that(
+    is.data.frame(ob),
+    isTRUE(
+      all(c("filename", "filepath", "file_ext") %in%
+        names(ob))
+    ),
+    nrow(ob) > 0
+  ))
 
 }
